76f9e333e5f1c6142f3f4cbc845bf2374d00d7a1
Vincent Do <vincentd4992@gmail.com>
2024-02-12 18:20:44 -0500

    Complete css for self description

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -9,31 +9,31 @@ const AboutMe = () => {
     return (
         <section id="aboutMe"> 
             <span className="aboutMeTitle">What I do</span>
-            <span className="aboutMeDescription">As a full-stack engineer, I bring expertise in React, JavaScript, HTML, CSS, Node.js, Express.js, and MongoDB. With proficiency across these technologies, I develop end-to-end solutions, integrating front-end interfaces with scalable back-end architectures. My commitment to staying updated ensures that I deliver efficient and innovative software solutions consistently.</span>
+            <span className="aboutMeDescription">As a full-stack engineer, I bring expertise in React, JavaScript, HTML, CSS, Node.js, Express.js, and MongoDB. With proficiency across these technologies, I develop end-to-end solutions, integrating front-end interfaces with scalable back-end architectures. My commitment to staying updated ensures that I consistently deliver efficient and innovative software solutions.</span>
             <div className="aboutMeBars">
                 <div className="aboutMeBar">
-                    <img className="skillBarImg" src={webDesign} alt='Skill Picture'></img>
+                    <img className="skillBarImg" src={webDesign} alt='Web Design'></img>
                     <div className="skillBarText">
                         <h2>Web Design</h2>
                         <p>description of skill</p>
                     </div>
                 </div>
                 <div className="aboutMeBar">
-                    <img className="skillBarImg" src={database} alt='Skill Picture'></img>
+                    <img className="skillBarImg" src={database} alt='Database Design'></img>
                     <div className="skillBarText">
                         <h2>Database Design</h2>
                         <p>description of skill</p>
                     </div>
                 </div>
                 <div className="aboutMeBar">
-                    <img className="skillBarImg" src={security} alt='Skill Picture'></img>
+                    <img className="skillBarImg" src={security} alt='Security Implementation'></img>
                     <div className="skillBarText">
                         <h2>Security Implementation</h2>
                         <p>description of skill</p>
                     </div>
                 </div>
                 <div className="aboutMeBar">
-                    <img className="skillBarImg" src={adaptation} alt='Skill Picture'></img>
+                    <img className="skillBarImg" src={adaptation} alt='Agile Adaptation'></img>
                     <div className="skillBarText">
                         <h2>Agile Adaptation</h2>
                         <p>description of skill</p>

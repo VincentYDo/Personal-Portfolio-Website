da5d33e2e4a1271be235a788c18afa41c5d42bb2
Vincent Do <vincentd4992@gmail.com>
2024-02-14 17:09:15 -0500

    Setup card detail data store

---
+++ src/components/Portfolio/portfolio.js
@@ -0,0 +1,84 @@
+import React from "react";
+import portfolio from './portfolio.css'
+
+
+// we want a pic, project name, description, tech stack
+const data = [
+    {   name: 'Kafkometry',
+        img: '../../assets/portfolioPics/kafkometryCardImg.jpeg',
+        description: 'A web application that allows users to track target metrics in their Kafka Clusters.',
+        techStack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', 'Kafka', 'Docker', 'MongoDB'],
+    },
+    {
+        name: 'Find-My-Fuel',
+        img: '../../assets/portfolioPics/findMyFuelCardImg.png',
+        description: 'A web application that allows users to find the nearest fuel stations to them.',
+        techStack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', 'Kafka', 'Docker', 'MongoDB'],
+    },
+    {
+        name: ' Frenchies',
+        img: '../../assets/portfolioPics/frenchiesCardImg.avif',
+        description: 'A web application that allows users to find local restaurants in their area.',
+        techStack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', 'Kafka', 'Docker', 'MongoDB'],
+    },
+    {
+        name: 'Scratch',
+        img: '../../assets/portfolioPics/scratchCardImg.png',
+        description: 'A web application that allows users to find local restaurants in their area.',
+        techStack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', 'Kafka', 'Docker', 'MongoDB'],
+    }
+
+]
+
+const Portfolio = ()=>{
+    return (
+        <section id="portfolio">
+            <span>My Portfolio</span>
+            <div className="cardsContainer">
+                <div className="card">
+                    <div className="cardImage">
+                        <img src="" alt="project pic"/>
+                    </div>
+                    <div className="cardText">
+                        <h2>Kafkometry</h2>
+                        <p>Project Description</p>
+                        <p>Tech Stack</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="cardImage">
+                        <img src="" alt="project pic"/>
+                    </div>
+                    <div className="cardText">
+                        <h2>Find-My-Fuel</h2>
+                        <p>Project Description</p>
+                        <p>Tech Stack</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="cardImage">
+                        <img src="" alt="project pic"/>
+                    </div>
+                    <div className="cardText">
+                        <h2>Frenchies</h2>
+                        <p>Project Description</p>
+                        <p>Tech Stack</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="cardImage">
+                        <img src="" alt="project pic"/>
+                    </div>
+                    <div className="cardText">
+                        <h2>Scratch</h2>
+                        <p>Project Description</p>
+                        <p>Tech Stack</p>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Portfolio;

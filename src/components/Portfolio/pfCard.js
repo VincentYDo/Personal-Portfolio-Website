d55a02641dd91a57e0f97a8ebffe6a796078b0f0
Vincent Do <vincentd4992@gmail.com>
2024-02-15 18:00:53 -0500

    Assemble structure of portfolio carousel

--- src/components/Portfolio/pfCard.js
+++ src/components/Portfolio/pfCard.js
@@ -1,10 +1,18 @@
 import React from "react";
+import './pfCard.css';
 
-const PfCard = ()=>{
+const PfCard = (props)=>{
 
     return(
-        <div>
-            First Card
+        <div className={props.cardId}>
+            <div className="cardImage">
+                <img src={props.img} alt="project pic"/>
+            </div>
+            <div className="cardText">
+                <h2>{props.name}</h2>
+                <p>{props.description}</p>
+                {/* <p>{props.techStack}</p> */}
+            </div>
         </div>
     )
 
--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -1,6 +1,12 @@
 import React from "react";
 import portfolio from './portfolio.css'
-import PfCarousel from "./pfCarousel.js";
+import PfCard from "./pfCard";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import kafkometryCardImg from '../../assets/portfolioPics/kafkometryCardImg.jpeg';
+import findMyFuelCardImg from '../../assets/portfolioPics/findMyFuelCardImg.png';
+import frenchieCardImg from '../../assets/portfolioPics/frenchieCardImg.jpeg';
+import scratchCardImg from '../../assets/portfolioPics/scratchCardImg.png';
 
 
 // we want a pic, project name, description, tech stack
@@ -8,78 +14,70 @@ import PfCarousel from "./pfCarousel.js";
 const Portfolio = ()=>{
     
     const data = [
-        {   name: 'Kafkometry',
-            img: require('../../assets/portfolioPics/kafkometryCardImg.jpeg'),
+        {   id:1,
+            name: 'Kafkometry',
+            img: kafkometryCardImg,
             description: 'A web application that allows users to track target metrics in their Kafka Clusters.',
             techStack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', 'Kafka', 'Docker', 'MongoDB'],
         },
-        {
+        {   id:2,
             name: 'Find-My-Fuel',
-            img: require('../../assets/portfolioPics/findMyFuelCardImg.png'),
+            img: findMyFuelCardImg,
             description: 'A web application that allows users to find the nearest fuel stations to them.',
             techStack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', 'Kafka', 'Docker', 'MongoDB'],
         },
-        {
+        {   id:3,
             name: ' Frenchies',
-            img: require('../../assets/portfolioPics/frenchieCardImg.jpeg'),
+            img: frenchieCardImg,
             description: 'A web application that allows users to find local restaurants in their area.',
             techStack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', 'Kafka', 'Docker', 'MongoDB'],
         },
-        {
+        {   id:4,
             name: 'Scratch',
-            img: require('../../assets/portfolioPics/scratchCardImg.png'),
+            img: scratchCardImg,
             description: 'A web application that allows users to find local restaurants in their area.',
             techStack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Express.js', 'Kafka', 'Docker', 'MongoDB'],
         }
     
     ];
+
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 1440 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 1440, min: 800 },
+          items: 4
+        },
+        tablet: {
+          breakpoint: { max: 800, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+    const cards = data.map(item =>(
+        <PfCard 
+        name={item.name} 
+        img={item.img} 
+        description={item.description} 
+        techStack={item.techStack} cardId={item.id}
+        />
+    ));
     
     return (
         <section id="portfolio">
             <span>My Portfolio</span>
             <div className="cardsContainer">
-                <PfCarousel/>
-
-                {/* <div className="card">
-                    <div className="cardImage">
-                        <img src="" alt="project pic"/>
-                    </div>
-                    <div className="cardText">
-                        <h2>Kafkometry</h2>
-                        <p>Project Description</p>
-                        <p>Tech Stack</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="cardImage">
-                        <img src="" alt="project pic"/>
-                    </div>
-                    <div className="cardText">
-                        <h2>Find-My-Fuel</h2>
-                        <p>Project Description</p>
-                        <p>Tech Stack</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="cardImage">
-                        <img src="" alt="project pic"/>
-                    </div>
-                    <div className="cardText">
-                        <h2>Frenchies</h2>
-                        <p>Project Description</p>
-                        <p>Tech Stack</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="cardImage">
-                        <img src="" alt="project pic"/>
-                    </div>
-                    <div className="cardText">
-                        <h2>Scratch</h2>
-                        <p>Project Description</p>
-                        <p>Tech Stack</p>
-                    </div>
-                </div> */}
+                <Carousel responsive={responsive} >
+                    {cards}
+                    
+                </Carousel>
             </div>
 
         </section>

3bdc603a8443e868916c5919a22a9526871d2d7a
Vincent Do <vincentd4992@gmail.com>
2024-06-26 17:48:22 -0400

    Add imgWrap functionality

--- src/components/Portfolio/pfCard.js
+++ src/components/Portfolio/pfCard.js
@@ -6,14 +6,12 @@ const PfCard = (props)=>{
     return(
         <div className="card">
             <div className={props.cardId}>
-                
-                <img className="projectImg" src={props.img} alt="project pic"/>
-                
-                {/* <div className="cardText">
+                <div className='imgWrap' >
+                    <img className="projectImg" src={props.img} alt="project pic"/>
                     <h2>{props.name}</h2>
                     <p>{props.description}</p>
                     <p>{props.techStack}</p>
-                </div> */}
+                </div>
             </div>
 
         </div>

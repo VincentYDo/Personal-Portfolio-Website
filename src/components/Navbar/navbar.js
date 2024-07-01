3e2dad81e561e51fcc04441fa357cbeea2520e18
Vincent Do <vincentd4992@gmail.com>
2024-07-01 18:16:39 -0400

    Add scroll top/bottom capability in navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './navbar.css';
-import {Link} from 'react-scroll'
+import {Link, Events, animateScroll as scroll} from 'react-scroll'
 import logo from '../../assets/logo.png';
 import contactImg from '../../assets/contactMeImg.png';
+
+const scrollToBottom = ()=>{
+    scroll.scrollToBottom();
+}
+
+const scrollToTop = ()=>{
+    scroll.scrollToTop();
+}
 
 
 const Navbar = ()=>{
@@ -10,7 +18,7 @@ const Navbar = ()=>{
         <nav className="navbar">
             <img src={logo}  alt="logo" className="logo"/>
             <div className="homepageMenu">
-                <Link className="homepageMenuItem">Home</Link>
+                <Link className="homepageMenuItem" onClick={scrollToTop}>Home</Link>
                 <Link className="homepageMenuItem">About</Link>
                 <Link className="homepageMenuItem">Portfolio</Link>
                 <Link className="homepageMenuItem">Clients</Link>

c51b31ebd6e0dd3ec310596c429e53d32396aff1
Vincent Do <vincentd4992@gmail.com>
2024-07-24 13:23:39 -0400

    Add img Icons that link to my social media pages

--- src/components/Links/links.js
+++ src/components/Links/links.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./links.css";
+import linkedinIcon from '../../assets/linkedinIcon.jpeg'
+import githubIcon from '../../assets/githubIcon.jpeg'
 
-const Links = ()=>{
+
+const Links = () => {
     return (
-        <div className="links">
-            <h1>Let's Connect</h1>
-                    <a href="https://www.linkedin.com/in/vincentydo/">LinkedIn</a>
-                    <a href="https://https://github.com/Vincent-Y-Do">Github</a>
-        </div>
+        <section className="links">
+            <div className="linksHeader">
+                <h1>Let's Connect</h1>
+            </div>
+            <div className="icons">
+                <a href="https://www.linkedin.com/in/vincentydo/" className="Icon">
+                    <img src={linkedinIcon} alt="linkedin" />
+                </a>
+                <a href="https://github.com/Vincent-Y-Do" className="Icon">
+                    <img src={githubIcon} alt="github"  />
+                </a>
+            </div>
+        </section>
     )
 }
 

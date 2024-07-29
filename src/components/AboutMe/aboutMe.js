834fccb6af23154d24b735248a6a8a18c9f60ecf
Vincent Do <vincentd4992@gmail.com>
2024-07-29 14:15:47 -0400

    Update database design description paragraph

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -21,12 +21,13 @@ const AboutMe = () => {
                     </div>
                 </div>
                 <div className="aboutMeBar">
-                    <img className="skillBarImg" src={database} alt='Database Design'></img>
                     <div className="skillBarText">
                         <h2>Database Design</h2>
                         <p>I have constructed SQL databases using PostgreSQL and NoSQL databases with MongoDB. 
-                            Additionally, I have designed database schemas to efficiently organize and structure data.</p>
+                            Additionally, I have designed database schemas to efficiently organize and structure data
+                            resulting in easier manipulation and querying of that data.</p>
                     </div>
+                    <img className="skillBarImg" src={database} alt='Database Design'></img>
                 </div>
                 <div className="aboutMeBar">
                     <img className="skillBarImg" src={security} alt='Security Implementation'></img>
@@ -38,12 +39,12 @@ const AboutMe = () => {
                     </div>
                 </div>
                 <div className="aboutMeBar">
-                    <img className="skillBarImg" src={adaptation} alt='Agile Adaptation'></img>
                     <div className="skillBarText">
                         <h2>Agile Adaptation</h2>
                         <p>Leveraging my expertise as a Professional Scrum Master, 
                             I have applied agile methodology to the development of numerous applications.</p>
                     </div>
+                    <img className="skillBarImg" src={adaptation} alt='Agile Adaptation'></img>
                 </div>
             </div>
 

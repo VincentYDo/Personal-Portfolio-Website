e0ddde5154ba26980e457919bbcd68189ddabd49
Vincent Do <vincentd4992@gmail.com>
2024-08-01 17:09:58 -0400

    Add baseline structure for techStack section

---
+++ src/components/TechStack/techStack.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './techStack.css'
+
+
+const techStack = () =>{
+    return(
+        <section id="techStack">    
+            <h1>Tech Stack</h1>
+            
+        </section>
+    )
+}
+
+export default techStack;

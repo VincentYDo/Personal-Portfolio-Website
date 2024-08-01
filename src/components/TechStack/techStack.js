a9f20e198fa270f88a8b52200f0b41e53f64a4c8
Vincent Do <vincentd4992@gmail.com>
2024-08-01 17:29:50 -0400

    Add baseline functioning react slick carousel and styling

--- src/components/TechStack/techStack.js
+++ src/components/TechStack/techStack.js
@@ -1,11 +1,43 @@
 import React from "react";
 import './techStack.css'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const techStack = () =>{
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 2000,
+        autoplay: true,
+        autoplaySpeed:2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        cssEase:'linear'
+    }
+
     return(
         <section id="techStack">    
             <h1>Tech Stack</h1>
+            <Slider {...settings}>
+              <div>
+                <h3>1</h3>
+            </div>
+            <div>
+            <h3>2</h3>
+            </div>
+            <div>
+            <h3>3</h3>
+            </div>
+            <div>
+            <h3>4</h3>
+            </div>
+            <div>
+            <h3>5</h3>
+            </div>
+                </Slider>
             
         </section>
     )

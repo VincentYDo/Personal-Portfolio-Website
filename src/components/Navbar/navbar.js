27bc49c4b75c19bb02ac4267573ac41996631e93
Vincent Do <vincentd4992@gmail.com>
2024-08-07 18:12:21 -0400

    Correct final positioning after clicking links on navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -14,10 +14,10 @@ const Navbar = ()=>{
         <nav className="navbar">
             <div className="homepageMenu">
                 <Link className="homepageMenuItem" onClick={scrollToTop}>Home</Link>
-                <Link className="homepageMenuItem" activeClass="active" to="aboutMe" spy={true} smooth={true} offset={-25}  duration={500}>About</Link>
-                <Link className="homepageMenuItem" activeClass="active" to="portfolio" spy={true} smooth={true} offset={-25}  duration={500}>Tech Stack</Link>
-                <Link className="homepageMenuItem" activeClass="active" to="portfolio" spy={true} smooth={true} offset={-25}  duration={500}>Portfolio</Link>
-                <Link className="homepageMenuItem" activeClass="active" to="links" spy={true} smooth={true} offset={-25}  duration={500}>Online Profiles</Link>
+                <Link className="homepageMenuItem" activeClass="active" to="aboutMe" spy={true} smooth={true} offset={0}  duration={500}>About</Link>
+                <Link className="homepageMenuItem" activeClass="active" to="techStack" spy={true} smooth={true} offset={0}  duration={500}>Tech Stack</Link>
+                <Link className="homepageMenuItem" activeClass="active" to="portfolio" spy={true} smooth={true} offset={0}  duration={500}>Portfolio</Link>
+                <Link className="homepageMenuItem" activeClass="active" to="links" spy={true} smooth={true} offset={0}  duration={500}>Online Profiles</Link>
             </div>
             <span></span>
         </nav>

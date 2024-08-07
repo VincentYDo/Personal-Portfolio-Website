8d90d9192c75522d8fc3c8620e6c0b60473daac2
Vincent Do <vincentd4992@gmail.com>
2024-08-07 17:44:12 -0400

    Remove Logo and CSS for Logo

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -12,10 +12,10 @@ const scrollToTop = ()=>{
 const Navbar = ()=>{
     return (
         <nav className="navbar">
-            <img src={logo}  alt="logo" className="logo"/>
             <div className="homepageMenu">
                 <Link className="homepageMenuItem" onClick={scrollToTop}>Home</Link>
                 <Link className="homepageMenuItem" activeClass="active" to="aboutMe" spy={true} smooth={true} offset={-25}  duration={500}>About</Link>
+                <Link className="homepageMenuItem" activeClass="active" to="portfolio" spy={true} smooth={true} offset={-25}  duration={500}>Tech Stack</Link>
                 <Link className="homepageMenuItem" activeClass="active" to="portfolio" spy={true} smooth={true} offset={-25}  duration={500}>Portfolio</Link>
                 <Link className="homepageMenuItem" activeClass="active" to="links" spy={true} smooth={true} offset={-25}  duration={500}>Online Profiles</Link>
             </div>
